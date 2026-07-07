b1266af945f2f1f85b497e8848c0022463049a13
najimunnisha2005 <najimunnisha2005@gmail.com>
2026-07-07 19:52:41 +0530

    Add files via upload

---
+++ week_3/SpringCore/Spring_Web_Project_Maven/spring-learn/src/main/java/com/springlearn/SpringLearnApplication.java
@@ -0,0 +1,19 @@
+package com.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Main method started...");
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        System.out.println("Application started successfully!");
+
+    }
+
+}
